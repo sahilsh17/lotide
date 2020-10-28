63eb4b0d1dd37950b4ee95736ca91fb91821cadc
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-10-28 22:22:30 +0000

    implemented letterPosition function

--- letterPositions.js
+++ letterPositions.js
@@ -1,7 +1,39 @@
-const letterPositions = function() {
+const letterPositions = function(inputString) {
   const results = {};
+  for (let i = 0; i < inputString.length; i++) {
+    if (results[inputString[i]]) {
+      results[inputString[i]].push(i);
+    } else {
+      results[inputString[i]] = [i];
+    }
+  }
 
 
   return results;
 
 };
+const assertArraysEqual = function(a,b) {
+  if (eqArrays(a,b)) {
+    console.log("✅✅✅Assertion Passed");
+  } else {
+    console.log("❌❌❌Assertion Failed");
+  }
+};
+const eqArrays = function(a,b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    
+    if (a[i] !== b[i]) {
+      return false;
+    }
+   
+  }
+  return true;
+};
+
+console.log(letterPositions("test"));
+console.log(letterPositions("sahil"));
+
+assertArraysEqual(letterPositions("sahil").h,[2]);
