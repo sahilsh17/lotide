0379ac8b04d6330216ff8f22503bbf3e3a54eb26
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-02 21:29:29 +0000

    transferred tests

---
+++ assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("./assertEqual");
+
+console.log(assertEqual("Lighthouse labs","bootcamp"));
+console.log(assertEqual(1,1));
+console.log(assertEqual(1,1));
+console.log(assertEqual("sahil","sahil"));
