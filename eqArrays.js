6d514b786e6179ec78c8126454814bbd571a6ef2
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-02 21:42:28 +0000

    exported function eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,16 @@
-const assertEqual = function(actual,expected) {
-
-  if (actual !== expected) {
-    return "❌❌❌Assertion Failed";
-  } else {
-    return "✅✅✅Assertion Passed";
-  }
-
-};
 
 const eqArrays = function(a,b) {
-  let c;
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] === b[i]) {
-      c = true;
-    } else {
-      c = false;
-    }
+  if (a.length !== b.length) {
+    return false;
   }
-  return c;
+  for (let i = 0; i < a.length; i++) {
+    
+    if(a[i]!==b[i]) {
+      return false;
+    }
+   
+  }
+  return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+module.exports = eqArrays;
