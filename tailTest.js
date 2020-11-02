1b6474130cd486ce40a711c1d35a1ea8ef881820
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-02 21:35:43 +0000

    tests transferred, used require

---
+++ tailTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require("./assertEqual");
+const tail = require("./tail.js");
+console.log(tail(["sahil","sharma","kevin"]));
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(assertEqual(result.length, 2)); // ensure we get back two elements
+console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
+console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
