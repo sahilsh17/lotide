bb137d441b69a72f7ec16d0b74956b14691a42f7
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-02 21:27:59 +0000

    exporting function head

--- head.js
+++ head.js
@@ -1,18 +1,4 @@
-const assertEqual = function(actual,expected) {
-
-  if(actual !== expected)
-  {
-    return "❌❌❌ Assertion Failed";
-  }
-  else{
-    return "✅✅✅ Assertion Passed";
-  }
-
-};
 const head = function(a){
   return a[0];
 }
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([5]), 5));
-console.log(head([]));
+module.exports = head;
