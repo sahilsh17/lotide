3ec821a7ff5d0280c914c46244b98191959a7c62
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-02 21:57:23 +0000

    exported functions

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = function(a,b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i = 0; i < a.length; i++) {
-    
-    if (a[i] !== b[i]) {
-      return false;
-    }
-   
-  }
-  return true;
-};
-const assertArraysEqual = function(a,b) {
-  if (eqArrays(a,b)) {
-    console.log("✅✅✅Assertion Passed");
-  } else {
-    console.log("❌❌❌Assertion Failed");
-  }
-};
+
 
 const middle = function(a) {
   let l = a.length;
@@ -29,12 +10,4 @@ const middle = function(a) {
     return [a[Math.ceil(l / 2) - 1]];
   } 
 };
-console.log(middle([1,2]));
-console.log(middle([1,2,3]));
-console.log(middle([1,2,3,4]));
-console.log(middle([1,2,3,4,5,6,7,8,9,10]));
-console.log(middle([1,2,3,4,5,6,7,8,9,10,11,12]));
-
-assertArraysEqual(middle([1,2,3,4,5,6,7]),[4]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]),[4,5]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,10,11,12]),[6,7]);
+module.exports = middle;
