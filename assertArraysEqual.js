721df840dbdc42498926eabc94a8e7d8267ff198
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-02 21:50:01 +0000

    exported function and used require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(a,b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i = 0; i < a.length; i++) {
-    
-    if(a[i]!==b[i]) {
-      return false;
-    }
-   
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(a,b) {
   if (eqArrays(a,b)) {
@@ -19,7 +7,4 @@ const assertArraysEqual = function(a,b) {
     console.log("❌❌❌Assertion Failed");
   }
 };
-
-assertArraysEqual([1,2,3,4],[1,2,3,4]);
-assertArraysEqual(['1','2','3',4],[1,2,3,4]);
-assertArraysEqual(['1','2','3','4'],['1','2','3','4']);
+module.exports = assertArraysEqual;
