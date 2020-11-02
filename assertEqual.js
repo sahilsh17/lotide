962350a9382b5a7fb1f64ea8570c0412c20b628e
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-02 21:27:22 +0000

    exported assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -2,15 +2,11 @@ const assertEqual = function(actual,expected) {
 
   if(actual !== expected)
   {
-    return "✅✅✅ Assertion Failed";
+    return "❌❌❌Assertion Failed";
   }
   else{
-    return "❌❌❌ Assertion Passed";
+    return "✅✅✅Assertion Passed";
   }
 
 };
-
-console.log(assertEqual("Lighthouse labs","bootcamp"));
-console.log(assertEqual(1,1));
-console.log(assertEqual(1,1));
-console.log(assertEqual("sahil","sahil"));
+module.exports = assertEqual;
