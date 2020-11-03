d644f18d1cc116505149a35149e39cc7343aa592
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-03 04:08:56 +0000

    updated code with export

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+
+
 const map = function(array,callback) {
   
 const results = [];
@@ -6,29 +8,5 @@ for(let item of array) {
 }
 return results;
 };
-const eqArrays = function(a,b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i = 0; i < a.length; i++) {
-    
-    if(a[i]!==b[i]) {
-      return false;
-    }
-   
-  }
-  return true;
-};
-const assertArraysEqual = function(a,b) {
-  if (eqArrays(a,b)) {
-    console.log("✅✅✅Assertion Passed");
-  } else {
-    console.log("❌❌❌Assertion Failed");
-  }
-};
 
-const words = ["ground", "control", "to", "major", "tom"];
-const results1 = map(words, word => word[0]);
-console.log(results1);
-console.log(map(["sahil","sharma","sylvester","stallone"],word => word[0]));
-assertArraysEqual(map(["sahil","sharma","sylvester","stallone"],word => word[0]),["s","s","s","s"]);
+module.exports = map;
