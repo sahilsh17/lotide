5d4eeadb694be9903cf01f119de2c8b159f71cfa
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-03 04:45:40 +0000

    updated code with export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual,expected) {
 
-  if (actual !== expected) {
-    return "❌❌❌Assertion Failed";
-  } else {
-    return "✅✅✅Assertion Passed";
-  }
-
-};
 const findKeyByValue = function(object,value) {
   for (let i in object) {
     
@@ -16,11 +8,6 @@ const findKeyByValue = function(object,value) {
   }
 
 };
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-console.log(findKeyByValue(bestTVShowsByGenre,"Brooklyn Nine-Nine"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
+
+
+module.exports = findKeyByValue;
