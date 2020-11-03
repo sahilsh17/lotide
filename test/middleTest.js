bcd3cde0e90aff8784695d2d26ad36785b022188
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-03 00:42:35 +0000

    modified code by using .deepEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -7,17 +7,17 @@ const assert = require('chai').assert;
 
 describe("#middle", () => {
   it("returns nothing for [1,2]", () => {
-    assert.strictEqual(assertArraysEqual(middle([]),[]), "✅✅✅Assertion Passed");
+    assert.deepEqual(middle([]),[]);
     assert
   });
   it("returns [2] for [1,2,3]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1,2,3]),[2]),"✅✅✅Assertion Passed"); 
+    assert.deepEqual(middle([1,2,3]),[2]); 
   });
   it("returns [2,3] for [1,2,3,4]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1,2,3,4]),[2,3]), "✅✅✅Assertion Passed"); 
+    assert.deepEqual(middle([1,2,3,4]),[2,3]); 
   });
   it("returns [5,6] for [1,2,3,4,5,6,7,8,9,10]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,10]),[5,6]), "✅✅✅Assertion Passed"); 
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8,9,10]),[5,6]); 
   });
 
 });
