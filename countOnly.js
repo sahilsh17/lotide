8859c3d8520d684c971c4cc0ae724f786db822be
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-03 05:15:05 +0000

    updated code to use export

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual,expected) {
-
-  if (actual !== expected) {
-    return "❌❌❌Assertion Failed";
-  } else {
-    return "✅✅✅Assertion Passed";
-  }
-
-};
 
 const countOnly = function(allItems,itemsToCount) {
 
@@ -22,21 +13,6 @@ const countOnly = function(allItems,itemsToCount) {
   }
   return result;
 };
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-console.log(assertEqual(result1["Jason"], 1));
-console.log(assertEqual(result1["Karima"], undefined));
-console.log(assertEqual(result1["Fang"], 2));
-console.log(assertEqual(result1["Agouhanna"], undefined));
+module.exports = countOnly;
