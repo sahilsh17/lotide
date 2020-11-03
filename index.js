7c80558c2db49eda7db36eb53af3b980832e6416
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-03 03:19:21 +0000

    file for using all lotide functions

---
+++ index.js
@@ -0,0 +1,28 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const map = require("./map");
+const letterPosition = require("./letterPositions");
+const findKeys = require("./findKeys");
+const findKeyByValue = require("./findKeyByValue");
+const eqObjects = require("./eqObjects");
+const eqArrays = require("./eqArrays");
+const countOnly = require("./countOnly");
+const countLetters = require("./countLetters");
+
+
+
+module.exports = 
+{
+  head: head,
+  tail: tail,
+  middle: middle,
+  map: map,
+  letterPosition : letterPosition,
+  findkeys: findKeys,
+  findKeyByValue: findKeyByValue,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  countOnly: countOnly,
+  countLetters: countLetters
+}
